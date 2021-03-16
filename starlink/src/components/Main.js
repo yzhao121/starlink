6b971b46b3a5ab4113a2156de492d94d2dcddb3f
yzhao121 <yzhao142857@gmail.com>
2021-03-15 22:57:45 -0400

    v0.0.1
    Project Initialization:
      Install dependencies;
      Build basic structure;
      Create Header, Footer and corresponding css file.

---
+++ starlink/src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render () {
+        return (
+            <div>
+                main
+            </div>
+        );
+    }
+}
+
+export default Main;
